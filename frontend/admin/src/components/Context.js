0f7b87ee743ac758fdb4f4b5eeb33f7981569d28
mavericktjj <64763339+mavericktjj@users.noreply.github.com>
2025-02-22 15:52:17 +0800

    login-signup-logic

--- frontend/admin/src/components/Context.js
+++ frontend/admin/src/components/Context.js
@@ -1,5 +1,5 @@
-import { createContext } from "react";
+// import { createContext } from "react";
 
-const Context = createContext(undefined)
+// const Context = createContext(undefined)
 
-export default Context
+// export default Context
--- frontend/admin/src/pages/Login.js
+++ frontend/admin/src/pages/Login.js
@@ -1,14 +1,52 @@
-import React, { useState } from 'react';
-import './LoginPage.css'; // Don't forget to add the CSS
+import React, { useState, useContext} from 'react';
+import './LoginPage.css';
+// import bcrypt from 'bcryptjs'
+import { useNavigate } from 'react-router-dom';
+import AuthContext from "../context/AuthProvider";
 
 const LoginPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate(); 
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // Handle login logic here (e.g., check username and password)
+     
+        if (!username || !password) {
+            
+            return;
+            
+        }
+        try {
+            const response = await fetch("http://localhost:3001/auth/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, password }),
+            });
+    
+            
+            const data = await response.json();
+    
+            if (!response.ok) {
+                console.error("Login failed:", data?.message || "Unknown error");
+                return;
+            }
+    
+            console.log("Login successful:", data);
+            login(username, data.data?.accessToken);
+
+            localStorage.setItem("username", data.data?.username)
+            localStorage.setItem("token", data.data?.accessToken); // Store token
+    
+        } catch (error) {
+            console.error("Error during login:", error.message);
+        }
+
+        alert("You have logged in successfully");
+
         console.log('Logging in with', { username, password });
+        navigate("/");
     };
 
     return (
