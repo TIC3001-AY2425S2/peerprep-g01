b9ad4fd1c342c04b2a8af8a6e1ec4686f4b006ec
unknown <lee.w.l@outlook.com>
2025-04-05 20:06:36 +0800

    changed docker files

--- collab-service/server.js
+++ collab-service/server.js
@@ -2,14 +2,8 @@ import http from "http";
 import index from "./index.js";
 import "dotenv/config";
 import { connectToDB } from "./model/repository.js";
-import { WebSocketServer } from 'ws';
-// import { createServer } from 'y-websocket';
-import * as Y from 'yjs';
-import { LeveldbPersistence } from 'y-leveldb'; // Optional: For persistent storage
 
-const persistence = new LeveldbPersistence('./leveldb')
 const port = process.env.PORT || 3004;
-
 const server = http.createServer(index);
 
 await connectToDB().then(() => {
@@ -20,22 +14,3 @@ await connectToDB().then(() => {
   console.error("Failed to connect to DB");
   console.error(err);
 });                
-
-
-// const wss = new WebSocketServer({ server });
-
-// const yServer = createServer({
-//   port: port, // You can specify the port here as well
-//   // Other options like persistence
-// });
-
-// wss.on('connection', (conn, req) => {
-//   yServer.handleConnection(conn, req);
-//   console.log('New WebSocket connection established.');
-// });
-
-// // Handle WebSocket connections
-// wss.on('connection', (conn, req) => {
-//   yServer.handleConnection(conn, req);
-//   console.log('New WebSocket connection established.');
-// });
---
+++ collab-y-websocket/app.js
@@ -0,0 +1,1 @@
+console.log(`run 'npx y-websocket'`);
