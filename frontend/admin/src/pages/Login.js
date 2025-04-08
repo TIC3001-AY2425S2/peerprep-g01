be6c89a361ec8627ba365cbd2c3ba4073e105d82
Denny Hee <e0895780@u.nus.edu>
2025-04-08 13:26:01 +0800

    UAT

--- frontend/admin/src/pages/Login.js
+++ frontend/admin/src/pages/Login.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import AuthContext from "../context/AuthProvider";
 
 const LoginPage = () => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const { login } = useContext(AuthContext);
     const navigate = useNavigate(); 
@@ -13,19 +13,17 @@ const LoginPage = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
      
-        if (!username || !password) {
-            
+        if (!email || !password) {
+            alert('Please enter both email and password');
             return;
-            
         }
         try {
             const response = await fetch("http://localhost:3001/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ username, password }),
+                body: JSON.stringify({ email, password }),
             });
     
-            
             const data = await response.json();
     
             if (!response.ok) {
@@ -36,19 +34,17 @@ const LoginPage = () => {
             }
     
             console.log("Login successful:", data);
-            login(username, data.data?.accessToken);
+            login(data.data?.username, data.data?.accessToken);
 
             localStorage.setItem("username", data.data?.username)
             localStorage.setItem("token", data.data?.accessToken); // Store token
-    
+            
+            alert("You have logged in successfully");
+            navigate("/");
         } catch (error) {
             console.error("Error during login:", error.message);
+            alert("Error during login: " + error.message);
         }
-
-        alert("You have logged in successfully");
-
-        console.log('Logging in with', { username, password });
-        navigate("/");
     };
 
     return (
@@ -56,12 +52,12 @@ const LoginPage = () => {
             <h2>Login</h2>
             <form onSubmit={handleSubmit} className="login-form">
                 <div className="input-group">
-                    <label htmlFor="username">Username</label>
+                    <label htmlFor="email">Email</label>
                     <input
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         required
                     />
                 </div>
